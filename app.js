d9a603d7a6c6801c295471922eb9bac5eac7d7ae
Roni Eka Setiawan <ronistwn35@gmail.com>
2023-01-07 16:26:02 +0700

    done setup and configuration

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 
 var app = express();
 
 // view engine setup
+app.use(cors)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
